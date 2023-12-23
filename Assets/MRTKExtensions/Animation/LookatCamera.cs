de9c9994f382df3181a4b9139a26fc56c776d9c3
Joost van Schaik <joostvanschaik@outlook.com>
2023-12-23 21:40:47 +0100

    Created first working sample

---
+++ Assets/MRTKExtensions/Animation/LookatCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace MRTKExtensions.Animation
+{
+    public class LookatCamera : MonoBehaviour
+    {
+        public float RotateAngle = 180f;
+
+        void Update()
+        {
+            gameObject.transform.LookAt(Camera.main.transform);
+            gameObject.transform.Rotate(Vector3.up, RotateAngle);
+        }
+    }
+}
---
+++ Assets/MRTKExtensions/Animation/LookatHorizontalDirection.cs
@@ -0,0 +1,19 @@
+﻿using MixedReality.Toolkit;
+using UnityEngine;
+
+namespace MRTKExtensions.Animation
+{
+    public class LookatHorizontalDirection : MonoBehaviour
+    {
+        public float RotateAngle = 180f;
+
+        void Update()
+        {
+            Vector3 targetPosition = new Vector3(Camera.main.transform.position.x,
+                transform.position.y,
+                Camera.main.transform.position.z);
+            this.transform.LookAt(targetPosition);
+            gameObject.transform.Rotate(Vector3.up, RotateAngle);
+        }
+    }
+}
