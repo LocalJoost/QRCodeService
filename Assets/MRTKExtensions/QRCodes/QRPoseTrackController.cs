9ab9f546e8598aad13a66f395f5cb5ad4823c584
Joost van Schaik <joostvanschaik@outlook.com>
2023-12-23 20:13:56 +0100

    Updated audio, actual show stuff

--- Assets/MRTKExtensions/QRCodes/QRPoseTrackController.cs
+++ Assets/MRTKExtensions/QRCodes/QRPoseTrackController.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
 
 namespace MRTKExtensions.QRCodes
 {
@@ -6,11 +8,14 @@ namespace MRTKExtensions.QRCodes
     {
         [SerializeField]
         private BaseTrackerController trackerController;
+        
+        private AudioSource audioSource;
 
         private Transform childObj;
 
         private void Start()
         {
+            audioSource = GetComponentInChildren<AudioSource>(true);
             childObj = transform.GetChild(0);
             childObj.gameObject.SetActive(false);
             trackerController.PositionSet.AddListener(PoseFound);
@@ -20,6 +25,22 @@ namespace MRTKExtensions.QRCodes
         {
             childObj.SetPositionAndRotation(pose.position, pose.rotation);
             childObj.gameObject.SetActive(true);
+            Task.Run(PlaySound);
+        }
+
+        private async Task PlaySound()
+        {
+            await Task.Yield();
+            if(audioSource != null && audioSource.clip != null)
+            {
+                audioSource.Play();
+            }
+        }
+
+        public void Reset()
+        {
+            trackerController.ResetTracking();
+            childObj.gameObject.SetActive(false);
         }
     }
 }
